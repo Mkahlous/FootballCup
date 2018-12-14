919af402d961fd204bd9dd44269b4d6ac1148f26
MKAHLOUS <mohamad.kahlous@capgemini.com>
2018-12-14 16:16:38 +0100

    Update

--- src/rouds/RoundsController.java
+++ src/rouds/RoundsController.java
@@ -6,28 +6,38 @@ import java.util.ArrayList;
 
 public class RoundsController {
 
-    public static void getWinningTeam(ArrayList<Teams> teams) {
-        //ArrayList<Teams>allTeams = new ArrayList<>();
+    public static ArrayList<Teams> getWinningTeam(ArrayList<Teams> teams) {
+        ArrayList<Teams> winners = new ArrayList<>();
         //All members.
         ArrayList<Teams> firstGroup = new ArrayList<>();
         //firstGroup contain 4 random teams
         ArrayList<Teams> secondGroup = new ArrayList<>();
         //secondGroup contain 4 random teams
         for (int i = 0; i < teams.size(); i = i + 2) {
+            firstGroup.add(teams.get(i));
+            secondGroup.add(teams.get(i + 1));
 
+          //  System.out.println(firstGroup.get(i).getClubName()+"Vs"+secondGroup.get(i).getClubName());
         }
 
+        for(int i = 0; i < firstGroup.size(); i++){
+            Teams winner = getWinner(firstGroup.get(i), secondGroup.get(i));
 
+            winners.add(winner);
+            System.out.println("Teh winner is " + " " +winner.getClubName());
+        }
+        System.out.println("******************");
+        return winners;
     }
-    public static void matchResult() {
-        int team1Goals = (int) Math.random();
-        int team2Goals = (int) Math.random();
 
-        if (team1Goals > team2Goals) {
-
+    public static Teams getWinner(Teams t1, Teams t2){
+        double team1 = Math.random();
+        double team2 = Math.random();
+        if(team1>team2){
+            return t1;
         }
-
-
-
+        else{
+            return t2;
+        }
     }
 }
